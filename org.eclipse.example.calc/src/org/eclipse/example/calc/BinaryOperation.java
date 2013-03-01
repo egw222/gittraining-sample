295e6051fb54c99eac6d079b40f8c1981d74873c
Marcel Friedmann <marcel.friedmann@de.markant.com>
2013-03-01 13:53:11 +0100

    test
    Change-Id: Idca194fcef7f1c68b446ec38bd3f3e0a14ed9f30

--- org.eclipse.example.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
  *
- * All rights reserved. This program and the accompanying materials
+ * Hacker
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
